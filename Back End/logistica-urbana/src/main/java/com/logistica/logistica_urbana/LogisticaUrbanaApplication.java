05881ee669913ef895c14518f11f18e007abf7bd
Juan Jose Tamayo Ospina <juanjose.tamayo@upb.edu.co>
2026-03-21 15:58:42 -0500

    Commit Inicial

---
+++ Back End/logistica-urbana/src/main/java/com/logistica/logistica_urbana/LogisticaUrbanaApplication.java
@@ -0,0 +1,13 @@
+package com.logistica.logistica_urbana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaUrbanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaUrbanaApplication.class, args);
+	}
+
+}
---
+++ Back End/logistica-urbana/src/test/java/com/logistica/logistica_urbana/LogisticaUrbanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistica.logistica_urbana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaUrbanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
